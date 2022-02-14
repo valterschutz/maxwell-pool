b8e4ba14b340421d1a818c95a3a7ca0e82e6e6b5
Valter Schütz <valterschutz@protonmail.com>
2022-02-14 16:50:17 +0100

    Plotting

--- main.m
+++ main.m
@@ -1,14 +1,21 @@
+que_ball.m = 0.1;  % 100 g
+que_ball.q = 0.01;  % 0.01 C
 
-m = 0.1;  % 100 g
-q = 0.01;  % 0.01 C
+% x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
+% [X,Y,Z] = meshgrid(x,y,z);
 
-x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
-[X,Y,Z] = meshgrid(x,y,z);
+que_ball.pos = [0.5; 0.5; 0.5];
+que_ball.v = [0.1; 0.1; 0.1];
 
-pos0 = [0.5; 0.5; 0.5];
+dt = 0.01;
+
+f = figure(1), grid on, xlim([0 1]), xlim('manual'), ylim([0 1]), ylim('manual'), zlim([0 1]), zlim('manual')
+for t=0:dt:5
+    que_ball.pos = que_ball.pos + que_ball.v*dt;
+    plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50)
+%     
+    pause(0.01)
+end
 
 
-plot3(pos0(1), pos0(2), pos0(3), '.', 'markersize', 50), hold on
-grid on, xlim([0 1]), ylim([0 1]), zlim([0 1])
-quiver3(X,Y,Z,
 
