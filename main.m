e85e6585e87e311104edaf1efddc2ab5618445f5
Valter Schütz <valterschutz@protonmail.com>
2022-02-15 10:56:52 +0100

    Initial code for moving particles with forces

--- main.m
+++ main.m
@@ -1,30 +1,38 @@
 que_ball.m = 0.1;  % 100 g
 que_ball.q = 1e-3;  % 1 mC
-
-x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
-[X,Y,Z] = meshgrid(x,y,z);
-
 que_ball.pos = [0; 0; 0];
 que_ball.v = [0.5; 0.5; 0.5];
-test_pos1 = [0.7;0.5;0.4];
-test_pos2 = [0.4;0.5;0.7];
-test_pos = [reshape(X,[1, numel(X)]); reshape(Y, [1, numel(Y)]); reshape(Z, [1, numel(Z)])];
-% test_pos is a matrix where each column is a position
-test_charge = 1e-3;
+
+test_ball1.m = 0.1;
+test_ball1.q = 1e-3;
+test_ball1.pos = [0.4;0.5;0.7];
+test_ball1.v = [0;0;0];
+
+test_ball2.m = 0.1;
+test_ball2.q = 1e-3;
+test_ball2.pos = [0.2;0.6;0.4];
+test_ball2.v = [0;0;0];
 
 dt = 0.1;
 
 for t=0:dt:2
     que_ball.pos = que_ball.pos + que_ball.v*dt;
-    plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z')
-    E_field = E(test_pos, que_ball);
-    B_field = B(test_pos, que_ball);
-%     F_E = E_field*test_charge;
-%     F_B = B_field*test_charge;
-%     E_field = E_field*2e-5;  % Scaling for plotting purposes
-    B_field = B_field*5e12;  % Scaling for plotting purposes
-%     quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), E_field(1,:),E_field(2,:),E_field(3,:), 'off'), hold off
-    quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), B_field(1,:),B_field(2,:),B_field(3,:), 'off'), hold off
+    test_ball1.E = E(test_ball1.pos, que_ball);
+    test_ball1.B = B(test_ball1.pos, que_ball);
+    test_ball1.F = test_ball1.q*(test_ball1.E + cross(test_ball1.v, test_ball1.B));
+    test_ball1.a = test_ball1.F/test_ball1.m;
+    test_ball1.v = test_ball1.v + test_ball1.a * dt;
+    test_ball1.pos = test_ball1.pos + test_ball1.v * dt;
+    test_ball2.E = E(test_ball2.pos, que_ball);
+    test_ball2.B = B(test_ball2.pos, que_ball);
+    test_ball2.F = test_ball2.q*(test_ball2.E + cross(test_ball2.v, test_ball2.B));
+    test_ball2.a = test_ball2.F/test_ball2.m;
+    test_ball2.v = test_ball2.v + test_ball2.a * dt;
+    test_ball2.pos = test_ball2.pos + test_ball2.v * dt;
+    
+    plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z'), hold on
+    plot3(test_ball1.pos(1), test_ball1.pos(2), test_ball1.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z')
+    plot3(test_ball2.pos(1), test_ball2.pos(2), test_ball2.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z'), hold off
     pause(0.01)
 end
 
--- plot_fields.m
+++ plot_fields.m
@@ -1,26 +1,22 @@
 que_ball.m = 0.1;  % 100 g
 que_ball.q = 1e-3;  % 1 mC
+
+x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
+[X,Y,Z] = meshgrid(x,y,z);
+
 que_ball.pos = [0; 0; 0];
 que_ball.v = [0.5; 0.5; 0.5];
-
-test_ball1.m = 0.1;
-test_ball1.q = 1e-3;
-test_ball1.pos = [0.4;0.5;0.7];
-test_ball1.v = [0;0;0];
-
-test_ball2.m = 0.1;
-test_ball2.q = 1e-3;
-test_ball2.pos = [0.2;0.6;0.4];
-test_ball2.v = [0;0;0];
+test_pos1 = [0.7;0.5;0.4];
+test_pos2 = [0.4;0.5;0.7];
+test_pos = [reshape(X,[1, numel(X)]); reshape(Y, [1, numel(Y)]); reshape(Z, [1, numel(Z)])];
+% test_pos is a matrix where each column is a position
+test_charge = 1e-3;
 
 dt = 0.1;
 
 for t=0:dt:2
     que_ball.pos = que_ball.pos + que_ball.v*dt;
     plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z')
-    test_ball1.E = E(test_ball1.pos, que_ball);
-    test_ball1.B = B(test_ball1.pos, que_ball);
-    test_ball1.F = test_ball1.q*(test_ball1.E + cross(test_ball1
     E_field = E(test_pos, que_ball);
     B_field = B(test_pos, que_ball);
 %     F_E = E_field*test_charge;
