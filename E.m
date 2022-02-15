725343c748cf0c221d406ce19ba0ab8227fd76b0
Valter Schütz <valterschutz@protonmail.com>
2022-02-15 09:56:33 +0100

    Moving charge and field arrow in one point

--- E.m
+++ E.m
@@ -1,20 +1,17 @@
 function res = E(test_pos,source_particle)
 % source_particle is a struct with attributes
-% pos: [x; y; z]
-% v: [vx; vy; vz]
-% q: int
-% test_pos is position [tx; ty; tz] of test charge
-% res is a column vector
-
-% [tx; ty; tz] = test_pos;
+%   pos: [x; y; z]
+%   v: [vx; vy; vz]
+%   q: int
+% test_pos is the position [tx; ty; tz] of test charge
+% res is a column vector containing components of E-field
 
 q = source_particle.q;
-% [vx; vy; vz] = source_particle.v;
 pos = source_particle.pos;
 
+epsilon_0 = 8.8541878128e-12;
+
 dist = norm(test_pos-pos);
-
 res = q/(4*pi*epsilon_0) * (test_pos-pos) * 1/dist.^3;
-
 end
 
--- main.m
+++ main.m
@@ -1,19 +1,22 @@
 que_ball.m = 0.1;  % 100 g
-que_ball.q = 0.01;  % 0.01 C
+que_ball.q = 1e-3;  % 1 mC
 
-% x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
-% [X,Y,Z] = meshgrid(x,y,z);
+x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
+[X,Y,Z] = meshgrid(x,y,z);
 
 que_ball.pos = [0.5; 0.5; 0.5];
 que_ball.v = [0.1; 0.1; 0.1];
+test_pos = [0.7;0.5;0.4];
+test_charge = 1e-3;
 
 dt = 0.01;
 
-f = figure(1), grid on, xlim([0 1]), xlim('manual'), ylim([0 1]), ylim('manual'), zlim([0 1]), zlim('manual')
-for t=0:dt:5
+for t=0:dt:0.5
     que_ball.pos = que_ball.pos + que_ball.v*dt;
-    plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50)
-%     
+    plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on
+    E_field = E(test_pos, que_ball);
+    F = E_field*test_charge;
+    quiver3(test_pos(1),test_pos(2),test_pos(3), F(1),F(2),F(3), 5e-7), hold off
     pause(0.01)
 end
 
