8510c628bf9aea9ae5382c01138cfa11f8a26b41
Valter SchÃhÃhS chütz <valterschutz@protonmail.com>
2022-02-16 11:29:10 +0100

    Add constants

--- main.m
+++ main.m
@@ -1,3 +1,9 @@
+DT = 0.01;
+T = 2;
+PAUSE_TIME = 0.001;
+E_SCALING = 1e-6;
+B_SCALING = 1e12;
+
 que_ball.m = 0.1;  % 100 g
 que_ball.q = 1e-3;  % 1 mC
 que_ball.pos = [0; 0; 0];
@@ -13,36 +19,36 @@ test_ball2.q = 1e-3;
 test_ball2.pos = [0.2;0.6;0.4];
 test_ball2.v = [0.01;-0.01;0.02];
 
-dt = 0.1;
 
-for t=0:dt:2
-    que_ball.pos = que_ball.pos + que_ball.v*dt;
+
+for t=0:DT:T
+    que_ball.pos = que_ball.pos + que_ball.v*DT;
     
-    test_ball1.E = E(test_ball1.pos, que_ball)*1e-6;
-    test_ball1.B = B(test_ball1.pos, que_ball)*1e12;
+    test_ball1.E = E(test_ball1.pos, que_ball)*E_SCALING;
+    test_ball1.B = B(test_ball1.pos, que_ball)*B_SCALING;
     test_ball1.F = test_ball1.q*(test_ball1.E+cross(test_ball1.v, test_ball1.B));
     test_ball1.a = test_ball1.F/test_ball1.m;
-    test_ball1.v = test_ball1.v + test_ball1.a * dt;
+    test_ball1.v = test_ball1.v + test_ball1.a * DT;
     if is_out_of_bounds(test_ball1.pos)
         test_ball1.v = -test_ball1.v;
     end
-    test_ball1.pos = test_ball1.pos + test_ball1.v * dt;
+    test_ball1.pos = test_ball1.pos + test_ball1.v * DT;
     
-    test_ball2.E = E(test_ball2.pos, que_ball)*1e-6;
-    test_ball2.B = B(test_ball2.pos, que_ball)*1e12;
+    test_ball2.E = E(test_ball2.pos, que_ball)*E_SCALING;
+    test_ball2.B = B(test_ball2.pos, que_ball)*B_SCALING;
     test_ball2.F = test_ball2.q*(test_ball2.E+cross(test_ball2.v, test_ball2.B));
     test_ball2.F = test_ball2.q*cross(test_ball2.v, test_ball2.B);
     test_ball2.a = test_ball2.F/test_ball2.m;
-    test_ball2.v = test_ball2.v + test_ball2.a * dt;
+    test_ball2.v = test_ball2.v + test_ball2.a * DT;
     if is_out_of_bounds(test_ball2.pos)
         test_ball2.v = -test_ball2.v;
     end
-    test_ball2.pos = test_ball2.pos + test_ball2.v * dt;
+    test_ball2.pos = test_ball2.pos + test_ball2.v * DT;
     
     plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z'), hold on
     plot3(test_ball1.pos(1), test_ball1.pos(2), test_ball1.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z')
     plot3(test_ball2.pos(1), test_ball2.pos(2), test_ball2.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z'), hold off
-    pause(0.001)
+    pause(PAUSE_TIME)
 end
 
 
--- plot_fields.m
+++ plot_fields.m
@@ -1,31 +1,41 @@
+PLOT_E = true;
+PLOT_B = false;
+E_SCALING = 2e-5;
+B_SCALING = 5e12;
+PAUSE_TIME = 0.01;
+DT = 0.1;
+T = 2;  % How long to run simulation
+
 que_ball.m = 0.1;  % 100 g
 que_ball.q = 1e-3;  % 1 mC
+que_ball.pos = [0; 0; 0];
+que_ball.v = [0.5; 0.5; 0.5];
 
 x = linspace(0,1,10); y = linspace(0,1,10); z = linspace(0,1,10);
 [X,Y,Z] = meshgrid(x,y,z);
 
-que_ball.pos = [0; 0; 0];
-que_ball.v = [0.5; 0.5; 0.5];
-test_pos1 = [0.7;0.5;0.4];
-test_pos2 = [0.4;0.5;0.7];
+
 test_pos = [reshape(X,[1, numel(X)]); reshape(Y, [1, numel(Y)]); reshape(Z, [1, numel(Z)])];
 % test_pos is a matrix where each column is a position
-test_charge = 1e-3;
+% test_charge = 1e-3;
 
-dt = 0.1;
 
-for t=0:dt:2
-    que_ball.pos = que_ball.pos + que_ball.v*dt;
+
+for t=0:DT:T
+    que_ball.pos = que_ball.pos + que_ball.v*DT;
     plot3(que_ball.pos(1), que_ball.pos(2), que_ball.pos(3), '.', 'markersize', 50), axis([0 1 0 1 0 1]), grid on, hold on, xlabel('x'), ylabel('y'), zlabel('z')
     E_field = E(test_pos, que_ball);
     B_field = B(test_pos, que_ball);
-%     F_E = E_field*test_charge;
-%     F_B = B_field*test_charge;
-    E_field = E_field*2e-5;  % Scaling
-%     B_field = B_field*5e12;  % Scaling
-    quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), E_field(1,:),E_field(2,:),E_field(3,:), 'off'), hold off
-%     quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), B_field(1,:),B_field(2,:),B_field(3,:), 'off'), hold off
-    pause(0.01)
+    E_field = E_field*E_SCALING;  % Scaling
+    B_field = B_field*B_SCALING;  % Scaling
+    if PLOT_E
+        quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), E_field(1,:),E_field(2,:),E_field(3,:), 'off')
+    end
+    if PLOT_B
+        quiver3(test_pos(1,:),test_pos(2,:),test_pos(3,:), B_field(1,:),B_field(2,:),B_field(3,:), 'off')
+    end
+    hold off
+    pause(PAUSE_TIME)
 end
 
 
