b2d78d3931b5be1ae22419dc918823d05c07f929
Valter Schütz <valterschutz@protonmail.com>
2022-03-08 17:32:04 +0100

    Aim feature

--- main_moving_charge.m
+++ main_moving_charge.m
@@ -6,7 +6,7 @@ max_speed = 1;  % This corresponds to user entering 100% speed when aiming
 
 que_ball.m = 1 * eye(2);  % 1 kg
 que_ball.q = 0.1e-3 * eye(2);  % 0.1 mC
-que_ball.x = vector_to_multivector([0; 0; 0]);
+que_ball.x = vector_to_multivector([0.5; 0.5; 0.5]);
 que_ball.v = vector_to_multivector([0.05; 0.05; 0.05]);
 que_ball.color = 'k';
 que_ball.F = 0;  % No interacting fields
@@ -29,13 +29,41 @@ end
 % myWriter.FrameRate = 30;
 % open(myWriter);
 
-% Plot initial positions
+% References to figure and axes
+figure
 fig = gcf;
 ax = gca;
-plot_particles([particles que_ball],ax)
 
-% Allow user to aim
-
+% Allow user to aim, start with aim [1;0;0]
+aim = [1;0;0];
+theta = pi/16;  % Small angle to rotate
+dz = 0.1;  % Small z to rotate upwards
+Z_rot = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
+Z_rot_inv = [cos(-theta) -sin(-theta) 0; sin(-theta) cos(-theta) 0; 0 0 1];
+while true
+    plot_particles(ax,[particles que_ball])
+    plot_aim(ax,que_ball,aim)
+    disp("Waiting for input...")
+    [~,~,button] = ginput(1);
+    switch button
+        case 28  % Left arrow
+            aim = Z_rot*aim;
+        case 29  % Right arrow
+            aim = Z_rot_inv*aim;
+        case 30  % Up arrow
+            aim = aim + [0;0;dz];
+            aim = aim/norm(aim);
+        case 31  % Down arrow
+            aim = aim - [0;0;dz];
+            aim = aim/norm(aim);
+        case 32  % Spacebar
+            break
+    end
+    
+end
+disp("Aim set!")
+strength = input("Hit strength (value between 0-100): "); 
+que_ball.v = vector_to_multivector(strength/100*max_speed*aim);
 
 k = 1;
 for t=0:DT:T
@@ -54,7 +82,7 @@ for t=0:DT:T
     que_ball.v = vector_to_multivector(bounce_check(multivector_to_vector(que_ball.x), multivector_to_vector(que_ball.v)));
     que_ball.x = que_ball.x + que_ball.v*DT;
     
-    plot_particles([particles que_ball],ax)
+    plot_particles(ax,[particles que_ball])
 %     view([k 25])
 %     set(gca,'nextplot','replacechildren');
 %     frame = getframe(gcf);
