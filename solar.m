07296ecd872b9a4d9c908b9cae76c0fab28eccbc
Valter SchÃhÃhS chütz <valterschutz@protonmail.com>
2022-04-27 10:18:36 +0200

    Update solar.m to use new variable names

--- solar.m
+++ solar.m
@@ -1,12 +1,21 @@
 clf, clearvars, clc
-DT = 0.004;
-T = 0.5;
-N_particles = 8;  % Number of particles
+DT = 0.1;
+T = 50;
 particle_colors = ['r','g','b','c','m','r','g','c'];
 MAX_RADII = 0.45;
-SMALLEST_PERIOD = 0.1;
+SMALLEST_PERIOD = 5;
 epsilon_0 = 8.8541878128e-12;
-save_frames = false;
+
+% Toggle this to see shadows for all particles on the sides of the box and
+% shadows for field object.
+PLOT_SHADOWS = false; 
+
+% Plot trajectory of particles
+PLOT_TRAJECTORY = false;
+
+% Save each frame in simulation and finally create a movie. Affects
+% performance drastically
+SAVE_MOVIE = false;
 
 % Planets not including Pluto
 % Orbital period in days
@@ -24,18 +33,18 @@ particle_distances = MAX_RADII*planet_distances/max(planet_distances);
 field_obj = generate_field_obj("charge");
 
 % Initialize particles
-for k=1:N_particles
-    particles(k).m = particle_masses(k);
+for k=1:length(planet_masses)
+    particles(k).mass = particle_masses(k);
     vy = 2*pi*particle_distances(k)/particle_orbital_periods(k);
-    particles(k).v = [0;vy;0];
-    particles(k).q = -particles(k).m*16*pi^3*epsilon_0*particle_distances(k)^3/(field_obj.q*particle_orbital_periods(k)^2);
+    particles(k).velocity = [0;vy;0];
+    particles(k).charge = -particles(k).mass*16*pi^3*epsilon_0*particle_distances(k)^3/(field_obj.charge*particle_orbital_periods(k)^2);
 %     fprintf("k=%d, q_solar=%.5e\n",k,particles(k).q(1,1))
-    particles(k).x = [0.5+particle_distances(k);0.5;0.5];
-    particles(k).F = 0 * eye(2);  % No field in beginning
+    particles(k).position = [0.5+particle_distances(k);0.5;0.5];
+    particles(k).field = 0 * eye(2);  % No field in beginning
     particles(k).force = 0;
-    particles(k).a = 0;
+    particles(k).acceleration = 0;
     particles(k).color = particle_colors(k);
-    particles(k).p = 0;  % Reference to plot object
+    particles(k).plotobj = 0;  % Reference to plot object
     particles(k).size = 20;
 end
 
@@ -45,19 +54,20 @@ clf(fig);
 ax = gca;
 ax = initialize_axes(ax);
 
-% Plot stuff
+% Zoom in on nearest 'planets'
 view([0 90]), axis([0.45 0.55 0.45 0.55 0 1]), set(gca,'DataAspectRatio',[1 1 1])
 
 % Plot particles and field object
-field_obj = plot_field_obj(ax,field_obj);
-particles = plot_particles(ax,particles);
+field_obj = plot_field_obj(ax,field_obj,PLOT_SHADOWS);
+particles = plot_particles(ax,particles,PLOT_SHADOWS);
 
-
-% Run the simulation
-frames = run_simulation_RK(field_obj,particles,T,DT,save_frames);
-if save_frames
-    writer = VideoWriter("videos/solar.avi");
-    open(writer);
-    writeVideo(writer,frames);
-    close(writer);
+% Run the simulation, save frames if SAVE_MOVIE flag is enabled
+if SAVE_MOVIE
+    frames = run_simulation_RK(ax,field_obj,particles,T,DT,PLOT_TRAJECTORY);
+    v = VideoWriter("videos/solar");
+    open(v);
+    writeVideo(v,frames);
+    close(v);
+else
+    run_simulation_RK(ax,field_obj,particles,T,DT,PLOT_TRAJECTORY);
 end
